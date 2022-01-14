2addc2a7d9246c55d1b8ad77f37783acb2dd158c
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-01-14 15:38:11 -0300

    update

--- negocios/files.test.js
+++ negocios/files.test.js
@@ -5,6 +5,10 @@ const fs = require('fs')
 const sinon = require('sinon')
 
 describe('lendo diretorio', () => {
+  afterEach(done => {
+    fs.readdir.restore()
+    done()
+  })
   it('diretorio lido', () => {
 sinon.stub(fs, 'readdir').callsFake((path, cb) => {
   cb(null, ['file1.txt'])
@@ -15,4 +19,15 @@ return files.readdir(path).then(list => {
   expect(fs.readdir.getCall(0).args[0]).to.equal(path)
 })
   })
+  it('falha lendo diretorio', () => {
+    sinon.stub(fs, 'readdir').callsFake((path, cb) => {
+      cb('error')
+    })
+    const path = './'
+    return files.readdir(path)
+    .catch(err => {
+      expect(err).to.equal('error')
+      expect(fs.readdir.getCall(0).args[0]).to.equal(path)
+    })
+      })
 })
